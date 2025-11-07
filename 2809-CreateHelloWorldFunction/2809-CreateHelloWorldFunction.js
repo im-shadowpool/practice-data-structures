2b7bfb97b5a96a23962e5b67e59e41c90abb161a
Saipavan Veeravalli <57819622+im-shadowpool@users.noreply.github.com>
2025-11-07 11:59:29 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,14 @@
+// Last updated: 11/7/2025, 11:59:29 AM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+//  const f = createHelloWorld();
+//  f(); // "Hello World"
+ 
